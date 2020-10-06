71b993e69b765c915ad14a68b91984616875153f
zakiyul <zakiyul.py@gmail.com>
2020-10-06 18:15:37 +0800

    update serializers with relations

--- keranjang/serializers.py
+++ keranjang/serializers.py
@@ -1,10 +1,12 @@
 from rest_framework import serializers
 from rest_framework import fields
 from .models import Keranjang
+from makanan.serializers import MakananSerializer
 
 
 class KeranjangSerializer(serializers.ModelSerializer):
-    Keranjang.makanan = serializers.RelatedField(many=True, read_only=True)
+    # Keranjang.makanan = serializers.RelatedField(many=True, read_only=True)
+    makanan = MakananSerializer()
 
     class Meta:
         model = Keranjang
